b1ab70a64d2971ef12e9abd96cc02c855d28cefd
Geoffrey Ballenden <7225070+ZA1@users.noreply.github.com>
2021-11-17 15:10:32 +1300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import QrReader from 'react-qr-reader'
+import base32Decode from 'base32-decode';
+import cose from './cose'
+import { Container, Card, Col, Row } from 'react-bootstrap';
+
+function App(key) {
+  const header = "NZCP:/1/";
+  //const data = 'NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX';
+  const [result, setResult] = useState({});
+  const keyRequest = fetch('https://nzcp.covid19.health.nz/.well-known/did.json');
+
+  // useEffect(() => {
+  //   decode(data)
+  // }, [])
+
+  function addBase32Padding(base32InputNoPadding) {
+    var result = base32InputNoPadding;
+    while ((result.length % 8) !== 0) {
+      result += '='
+    }
+    return result;
+  }
+
+  function decode(data) {
+    if (!data) return;
+    try
+    {
+      const encoded = data.replace(header, '');
+      const decoded = base32Decode(addBase32Padding(encoded), "RFC4648");
+
+      keyRequest
+        .then(response => response.json())
+        .then(pk => cose.parseCose(decoded, pk))
+        .then(r => setResult(r));
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+  function handleScan(data) {
+    if (data) {
+      if(data.startsWith(header))
+      {
+        decode(data);
+      }
+    }
+  }
+
+  function handleError(err) {
+    console.error(err);
+  }
+
+  function StartCard(data) {
+    return (<Card>
+      <Card.Header>
+      <h5 class="card-title">NZ Covid Cert</h5>
+      </Card.Header>
+      <Card.Body>
+        <p>Please scan the QR Code</p>
+      </Card.Body>
+    </Card>)
+  }
+
+  function DisplayValue(props) {
+    return(
+      <div className="ms-2 me-auto">
+        {props.label}
+        <div className="fw-bold">{props.value}</div>
+      </div>
+    )
+  }
+
+  function ValidCard(props) {
+    const details = props.payload.vc.credentialSubject;
+    const name = details.givenName + " " + details.familyName;
+    const dob = details.dob;
+    return (<Card bg="success" text="white">
+      <Card.Header>
+        <h5 class="card-title">Valid Certificate</h5>
+      </Card.Header>
+      <Card.Body>
+          <DisplayValue label="Name" value={name} ></DisplayValue>
+          <DisplayValue label="Date of Birth" value={dob} ></DisplayValue>
+      </Card.Body>
+    </Card>)
+  }
+
+  function InvalidCard() {
+    return (<Card bg="danger" text="white">
+      <Card.Header>
+        <h5 class="card-title">Invalid Certificate</h5>
+      </Card.Header>
+      <Card.Body>
+          <p>This certificate is invalid. Please do not accept it.</p>
+      </Card.Body>
+    </Card>)
+  }
+
+  function InfoCard(props) {
+    if(props.data && props.data.signature) {
+      if(props.data.signature.valid) {
+        return ValidCard(props.data);
+      } else {
+        return InvalidCard(props.data);
+      }
+    }
+
+    return StartCard(props.data);
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col md={6}>
+          <QrReader
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ width: '100%' }}
+          />
+        </Col>
+        <Col md={6}>
+          <InfoCard data={result} />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/cose.js
@@ -0,0 +1,163 @@
+import cbor from 'cbor'
+
+const headerLabels = {
+    "4": {
+        name: "kind",
+        parse: function(value) {
+            return value;
+        }
+    },
+    "1": {
+        name: "alg",
+        parse: function(value) {
+            switch(value) {
+                case "-7": return "ECDSA_w_SHA256";
+                case "-8": return "EdDSA";
+                case "-35": return "ECDSA_w_SHA384";
+                case "-36": return "ECDSA_w_SHA512";
+                case "-257": return "RSASSA-PKCS1-v1_5_w_SHA256";
+                case "-258": return "RSASSA-PKCS1-v1_5_w_SHA384";
+                case "-259": return "RSASSA-PKCS1-v1_5_w_SHA512";
+                case "-65535": return "RSASSA-PKCS1-v1_5_w_SHA1";
+                default: return value;
+            }
+        }
+    }
+}
+
+const payloadLabels = {
+    "1": {
+        name: "iss",
+        parse: v => v.toString()
+    },
+    "4": {
+        name: "exp",
+        parse: v => v
+    },
+    "5": {
+        name: "nbf",
+        parse: v => v
+    },
+    "7": {
+        name: "jti",
+        parse: v => Array.from(v)
+    }
+}
+
+function parseHeader(parsedCose) {
+    const headerCbor = parsedCose.value[0];
+    if(!(headerCbor instanceof Uint8Array)) {
+        throw new Error("Invalid header");
+    }
+    
+    const parsedHeader = cbor.decode(headerCbor);
+    if(!(parsedHeader instanceof Map)) {
+        throw new Error("Invalid decoded header");
+    }
+
+    let header = {};
+    for (let kv of parsedHeader) {
+        let key = kv[0].toString();
+        let value = kv[1].toString();
+
+        let item = headerLabels[key];
+        if(item) {
+            header[item.name] = item.parse(value);
+        }
+    }
+
+    return Promise.resolve(header);
+}
+
+function parsePayload(parsedCose) {
+    var parsedPayload = cbor.decode(parsedCose.value[2]);
+
+    let payload = {};
+    for (let kv of parsedPayload) {
+        let key = kv[0].toString();
+        let value = kv[1];
+
+        let item = payloadLabels[key];
+        if(item) {
+            payload[item.name] = item.parse(value);
+        } else {
+            payload[key] = value;
+        }
+    }
+    return Promise.resolve(payload);
+}
+
+function parseSignature(parsedCose, key) {
+    let valid = false;
+    if(parsedCose.tag === 18)
+    {
+        const header = parsedCose.value[0];
+        const plaintext = parsedCose.value[2];
+        const externalAAD = key.externalAAD || Buffer.alloc(0);
+        const SigStructure = [
+            'Signature1',
+            header,
+            externalAAD,
+            plaintext
+          ];
+        const sig = cbor.encode(SigStructure);
+        
+        return crypto.subtle.importKey(
+            "jwk",
+            key.verificationMethod[0].publicKeyJwk,
+            {
+                name: "ECDSA",
+                namedCurve: "P-256"
+              },
+            false,
+            ["verify"])
+        .then(publicKey => crypto.subtle.verify({
+                    name: "ECDSA",
+                    hash: {name: "SHA-256"},
+                }, 
+                publicKey,
+                parsedCose.value[3],
+                sig)
+        )
+        .then(valid => {return {
+            valid,
+            sig: parsedCose.value[3]
+        }})
+    }
+
+    Promise.resolve({
+        valid,
+        sig: parsedCose.value[3]
+    });
+}
+
+function parseCose(cose, key) {
+    var parsedCose;
+    try {
+        parsedCose = cbor.decode(cose);
+        if(parsedCose.err) {
+            throw parsedCose.err;
+        }
+        
+        return Promise.all([
+            parseHeader(parsedCose),
+            parsePayload(parsedCose),
+            parseSignature(parsedCose, key)]
+        ).then(r => { 
+            const [header, payload, signature] = r;
+            return {
+                header, 
+                payload, 
+                signature
+            }
+        });
+
+    } catch (err) {
+        throw new Error("Error parsing cose: " + err);
+    }
+}
+
+const exports = {
+    parseCose
+}
+export default exports;
